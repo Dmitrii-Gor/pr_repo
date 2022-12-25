c58f1a3c53d91767f5e0e5f0bcb33d80fe88965c
Dmitrii Gor <dima.goryunenko@mail.ru>
2022-12-25 12:04:24 +0300

    inital commit

---
+++ dirty_main.py
@@ -0,0 +1,5 @@
+import application.salary
+# from application import * как тогда должна работать данная строка? Если при вызове функции, автоматически вылезает импорт сверху
+# т.е. я импортирую всё со зведой, но не получаю ничего
+
+application.salary.calculate_salary()
